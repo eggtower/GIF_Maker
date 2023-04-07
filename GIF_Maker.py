86524591747313b2262cfc682343c2d469690a10
eggtower <johnson870930@gmail.com>
2023-04-08 02:44:39 +0800

    feat: add frame count ; enhance brightness/color; adjust duration; frame collect qual

--- GIF_Maker.py
+++ GIF_Maker.py
@@ -3,36 +3,35 @@ import pathlib
 import shutil
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, ImageEnhance
 
-def make_gif(clipPath, savePath):
+def make_gif(clipPath, savePath, q):
     frameList = list()
     
     clip = cv2.VideoCapture(clipPath)
-    # fps = clip.get(5) # 影片 fps
-    frames = int(clip.get(cv2.CAP_PROP_FRAME_COUNT)) # 影片總幀數
-    width, height = clip.get(3), clip.get(4) # 影片寬度,高度
-    reSize = (int(width/height*450), 450) # 調整大小, 高固定 450
+    fps = clip.get(5)
+    width, height = clip.get(3), clip.get(4)
+    reSize = (int(width/height*450), 450) # reshape, fixed height 450
     
-    for i in range(frames):
+    # for i in range(frames):
+    frameCnt = 0
+    while clip.isOpened():
         ret, frame = clip.read()
         if not ret: break
             
-        if i%4==0:
+        if frameCnt%q==0:
             frame = cv2.resize(frame, reSize)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGBA)
-            # 圖像增強
-            frame = 255*np.power(frame/255, 1.136)
-            frame = np.around(frame)
-            frame[frame>255] = 255
-            frame = frame.astype(np.uint8)
+            # Convert to a format that PIL can handle
+            frame = Image.fromarray(np.uint8(frame))
+            frame = ImageEnhance.Brightness(frame).enhance(1.006)
+            frame = ImageEnhance.Color(frame).enhance(1.158)
             
-            frame = Image.fromarray(frame)
-            frame = frame.convert('RGB') 
             frameList.append(frame)
+        frameCnt+=1
     clip.release()
     frameList[0].save(savePath, format="GIF", append_images=frameList[1:],
-                      save_all=True, duration=4, loop=0, disposal=2)
+                      save_all=True, duration=1000/fps*q, loop=0, disposal=2)
                       
 def main():
     if not os.path.exists('clips'):
@@ -57,7 +56,7 @@ def main():
         print('Processing..', file)
         clipPath = os.path.join(clipsDir, file)
         savePath = '{0}/{1}_{2}.gif'.format(saveDir, inputDate, cnt)
-        make_gif(clipPath, savePath)
+        make_gif(clipPath, savePath, 1)
         cnt += 1
     cv2.destroyAllWindows()
     input('Press any key to end.')
